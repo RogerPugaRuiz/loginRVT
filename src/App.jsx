0d7bf5d0d857b38a145a681f3e73d26c4299f8e1
RogerPugaRuiz <roger@ancoradual.com>
2023-12-11 14:24:38 +0100

    Refactor class attributes to className in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,36 +10,36 @@ function App() {
   }
   return (
   <>
-      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" className="container">
-        <img class="mx-auto h-10 w-auto" src={logo} alt="Logo React" />
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900" >Magane your account</h2>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" >
+        <img className="mx-auto h-10 w-auto" src={logo} alt="Logo React" />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900" >Magane your account</h2>
         </div>
 
-        <form class = "text-center" onSubmit={(e) => handleSubmit(e)}>
+        <form className = "text-center" onSubmit={(e) => handleSubmit(e)}>
 
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8"className="inputContainer">
-          <p class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Email</p>
-          <input class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <p className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Email</p>
+          <input className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           type="text" />
         </div>
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" className="inputContainer">
-          <p class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Password</p>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" >
+          <p className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Password</p>
           <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           type="password" />
         </div>
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" className="bottomForm">
-          <button class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900" type="submit">Login</button>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" >
+          <button className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900" type="submit">Login</button>
           <div className="links">
-            <p class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">New User?</p>
-            <p class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Forgot Password?</p>
+            <p className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">New User?</p>
+            <p className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Forgot Password?</p>
           </div>
           </div>
         </form>
-        <small class="text-center" className="copyright">&copy react</small>
+        <small className="text-center">&copy react</small>
         
         </>
   );
